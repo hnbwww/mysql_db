c14f029144934eb2fdf0c3a90b9efc657329ba27
hnbwww <52497032+hnbwww@users.noreply.github.com>
2019-09-03 09:32:07 +0800

    Update db.class.php

--- db.class.php
+++ db.class.php
@@ -167,6 +167,19 @@ class Db{
     }
     return $res;
   }
+
+  /**
+   * 执行sql语句
+   *
+   * @param   <type>  $sql  The sql
+   *
+   * @return   boolean ( description_of_the_return_value )
+   */
+  public function query($sql=null){
+    $res = $this->execute($sql);
+    return $res;
+  }  
+  
   /**
    * 插入数据
    *
@@ -240,15 +253,19 @@ class Db{
     }
   }
 }
-$db = new Db();
-//查询操作
-var_dump($db->table('user')->where('id > 2')->order('id desc')->limit('2,4')->select());
-//插入操作
-var_dump($db->table('user')->insert(array('username'=>'user','password'=>'pwd')));
-//更新操作
-var_dump($db->table('user')->where('id = 1')->update(array('username'=>'user1','password'=>'pwd1')));
-//删除操作
-var_dump($db->table('user')->where('id = 1')->delete());
+
+
+//$db = new Db();
+
+// //查询操作
+// var_dump($db->table('user')->where('id > 2')->order('id desc')->limit('2,4')->select());
+// //插入操作
+// var_dump($db->table('user')->insert(array('username'=>'user','password'=>'pwd')));
+// //更新操作
+// var_dump($db->table('user')->where('id = 1')->update(array('username'=>'user1','password'=>'pwd1')));
+// //删除操作
+// var_dump($db->table('user')->where('id = 1')->delete());
+
 
 
 
